37ca203af87daed28a411073a430d72db19a1d1b
MasWho <xm.hu.wk@gmail.com>
2022-09-24 18:14:43 +0200

    created custom hook for splitting out blog fetching logic

---
+++ src/hooks/useBlogsData.ts
@@ -0,0 +1,62 @@
+import { useState, useCallback, useEffect } from "react";
+import BlogData, {RawBlogData} from '../model/blogData';
+
+interface useBlogs {
+  blogs?: BlogData[];
+  loading: boolean;
+  error: string;
+};
+
+/**
+ * contains business logic for fetching data from a provided endpoint blogsUrl.
+ * Consists of a data state blogs, loading and error states which can be used in presentation component calling this hook.
+ * @param blogsUrl 
+ * @returns 
+ */
+const useBlogsData = (blogsUrl: string): useBlogs => {
+  const [blogs, setBlogs] = useState<BlogData[]>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>('');
+
+  const fetchBlogsData = useCallback(
+    async () => {
+      setLoading(true);
+      setError('');
+
+      try {
+        const response = await fetch(blogsUrl);
+        if (!response.ok) {
+          const data = await response.json();
+          throw new Error(data.error);
+        }
+
+        // Transform raw blog data to format consumed by presentation
+        const data: RawBlogData[] = await response.json();
+        const blogs: BlogData[] = [];
+        data.forEach(datum => {
+          const blog = BlogData.fromJsonResponse(datum);
+          blogs.push(blog);
+        })
+
+        setBlogs(blogs);
+      } catch (error: any) {
+        console.error(error);
+        setError("Something went wrong, please contact support");
+      }
+      
+      setLoading(false);
+    }, [blogsUrl]
+  );
+
+  useEffect(() => {
+    fetchBlogsData();
+  }, [fetchBlogsData]);
+
+  return {
+    blogs,
+    loading,
+    error
+  };
+};
+
+export default useBlogsData;
